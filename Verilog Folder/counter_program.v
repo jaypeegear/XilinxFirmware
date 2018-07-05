736af996e651c8e2c3472a8d50bcf648b769ea43
jaypeegear <40642336+jaypeegear@users.noreply.github.com>
2018-07-05 15:16:57 -0700

    Add files via upload
    The changes to counter_program includes a clock divider that allows for the counter to count at a frequency of 27 hertz, a reg to keep track of whether or not to engage the divider, a name change for the lower_clk to khz_clk, and another counter and temporary register. The constraints file has the changes of having the lower_clk name changed to khz_clk, and a port dedicated to the hz_clk.

--- Verilog Folder/counter_program.v
+++ Verilog Folder/counter_program.v
@@ -26,7 +26,8 @@ module counter_program(
 	output rfd,
 	output [15:0] quotient,
 	output [15:0] fractional,
-	output lower_clk
+	output khz_clk,
+	output hz_clk
 	);
 
 reg [15:0] count = 16'b000000000000000;
@@ -35,8 +36,10 @@ reg [15:0] count2 = 16'b000000000000000;
 reg [15:0] tempc2 = 16'b000000000000000;
 reg [15:0] sample =16'b000000000000000;
 reg [15:0] temple = 16'b000000000000000;
-reg lower_clk = 1'b0;
-
+reg khz_clk = 1'b0;
+reg hz_clk = 1'b0;
+reg [15:0] count3 = 16'b000000000000000;
+reg [15:0] tempc3 = 16'b000000000000000;
 div divider (
 	.clk(clk), // input clk
 	.rfd(rfd), // output rfd
@@ -49,7 +52,7 @@ div divider (
 
 always @ (posedge sig)
 begin
-	if (count != 16'b1111111111111111)
+	if (count != 16'b0000000000111111 )
 	begin
 		temple = sample + 1;
 		sample = temple;
@@ -59,55 +62,110 @@ begin
 		temple = temple;
 		sample = sample;
 	end
-	if (sample <= 16'b000000000000001 && count == 16'b1111111111111111 && lower_clk != 1)
+	if (fractional <= 16'b000000000010000 && count == 16'b0000000000111111  && khz_clk == 1'b1 && hz_clk != 1'b1)
 	begin
-	sample = 16'b000000000000000;
-	end
-	/*if (sample > 16'b1111111111111111)
-	begin
-	sample = 16'b000000000000000;
-	temple = 16'b000000000000000;
+	sample = 16'b0000000000000000;
+	temple = 16'b0000000000000000;
 	end
 	else
 	begin
-	sample = sample;
-	temple = temple;
+		if (fractional <= 16'b000000000010000 && count == 16'b0000000000111111 && khz_clk != 1'b1 && hz_clk == 1'b1)
+		begin
+			sample = 16'b0000000000000000;
+			temple = 16'b0000000000000000;
+		end
+		else
+		begin
+			sample = sample;
+			temple = temple;
+		end
 	end
-	sig_bit = sample[15:12];*/
+	if (khz_clk == 1'b1 && hz_clk == 1'b1)
+	begin
+		sample = 16'b0000000000000000;
+		temple = 16'b0000000000000000;
+	end
+	else
+	begin
+		sample = sample;
+		temple = temple;
+	end
 end
 
 always @ (posedge clk)
 begin
-	if (count !=  16'b1111111111111111 && sample != 16'b1111111111111111 && lower_clk != 1'b1)
+	if (count !=  16'b0000000000111111 && sample != 16'b0000000000111111 && sample!=16'b0000000000000000 && khz_clk != 1'b1 && hz_clk != 1'b1)
 	begin
 		tempc = count + 1;
 		count = tempc;
 	end
 	else
 	begin
-		if (sample <= 16'b000000000000001 && count == 16'b1111111111111111 && lower_clk != 1)
+		if (fractional <= 16'b0000000000010000 && sample!=16'b0000000000000000 && count == 16'b0000000000111111  && khz_clk != 1'b1 && hz_clk != 1'b1)
 		begin
-			tempc = 16'b000000000000000;
-			count = 16'b000000000000000;
-			lower_clk = 1'b1;
+			tempc = 16'b0000000000000000;
+			count = 16'b0000000000000000;
+			khz_clk = 1'b1;
 		end
 		else
 		begin
-			tempc = tempc;
-			count = count;
+			if (fractional <= 16'b0000000000010000 && sample!=16'b0000000000000000 && count == 16'b0000000000111111  && khz_clk == 1'b1 && hz_clk != 1'b1)
+			begin
+				tempc = 16'b0000000000000000;
+				count = 16'b0000000000000000;
+				khz_clk = 1'b0;
+				hz_clk = 1'b1;
+			end
+			else
+			begin
+				if (fractional <= 16'b0000000000010000 && sample!=16'b0000000000000000 && count == 16'b0000000000111111 && khz_clk != 1'b1 && hz_clk == 1'b1)
+				begin
+					tempc = 16'b0000000000000000;
+					count = 16'b0000000000000000;
+					khz_clk = 1'b1;
+					hz_clk = 1'b1;
+				end
+				else
+				begin
+					tempc = tempc;
+					count = count;
+				end 
+			end 
 		end
 	end
 	
-	if (lower_clk == 1'b1)
+	if (khz_clk != 1'b1 && hz_clk == 1'b1)
 		begin
-			tempc2 = count2 + 1;
-			count2 = tempc2;
-			if (count2 == 16'b0000001111101000 && count != 16'b1111111111111111)
+			if (count2 != 16'b0000001111101000)
+			begin
+				tempc2 = count2 + 1;
+				count2 = tempc2;
+			end
+			else
+			begin
+				tempc2 = tempc2;
+				count2 = count2;
+			end
+			if (count2 == 16'b0000001111101000 && count != 16'b0000000000111111 )
 			begin
 				tempc2 = 16'b000000000000000;
 				count2 = 16'b000000000000000;
-				tempc = count + 1;
-				count = tempc;
+				tempc3 = count3 + 1;
+				count3 = tempc3;
+				if (count3 == 16'b0000001111101000 && count !=16'b0000000000111111 )
+				begin
+					tempc3 = 16'b000000000000000;
+					count3 = 16'b000000000000000;
+					tempc = count + 1;
+					count = tempc;
+				end
+				else
+				begin
+				tempc = tempc;
+				count = count;
+				tempc3 = tempc3;
+				count3 = count3;
+				end
 			end
 			else
 			begin
@@ -122,41 +180,42 @@ begin
 			tempc = tempc;
 			count = count;
 		end
-	
-//0000 0011 1110 1000
-//	if(switch1==1)
-//	begin
-//		tempc = tempc;
-//		count = count;
-//	end
-	
-	/*if (count == 16'b1111111111111111)
+		
+	if (khz_clk == 1'b1 && hz_clk != 1'b1)
 	begin
-		count = 16'b000000000000000;
-		tempc = 16'b000000000000000;
+		if (count2 != 16'b0000001111101000)
+		begin
+			tempc2 = count2 + 1;
+			count2 = tempc2;
+		end
+		else
+		begin
+			tempc2 = tempc2;
+			count2 = count2;
+		end
+		if (count2 == 16'b0000001111101000 && count != 16'b0000000000111111)
+		begin
+			tempc2 = 16'b000000000000000;
+			count2 = 16'b000000000000000;
+			tempc = count + 1;
+			count = tempc;
+		end
+		else
+		begin
+			tempc2 = tempc2;
+			count2 = count2;
+			tempc = tempc;
+			count = count;
+		end
 	end
 	else
 	begin
-			tempc = count + 1;
-			count = tempc;
-	end*/
-//	else begin
-//		tempc = tempc;
-//		count = count;
-//	end
-//	if(switch2==1)
-//	begin
-//		led = 4'b0000;
-//		templ = 4'b0000;
-//	end
+		tempc = tempc;
+		count = count;
+	end
 end
 
-
 endmodule
-
-
-
-
 //		if (count == 32'b00000000000000000000000000000001)
 //		begin
 //			tempc = 32'b00000000000000000000000000000000;
